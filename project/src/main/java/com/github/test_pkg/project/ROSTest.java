48b3b5efd3834bdbeedc18dd5ed51190927c34aa
rodrigo <rjcausarano@gmail.com>
2019-04-04 09:20:28 -0400

    This commit is meant to be tested with branch test-fix-proposal of the rosjava_core repo.

--- project/src/main/java/com/github/test_pkg/project/ROSTest.java
+++ project/src/main/java/com/github/test_pkg/project/ROSTest.java
@@ -6,8 +6,10 @@ import org.ros.node.NodeConfiguration;
 import org.ros.node.NodeMain;
 import org.ros.node.NodeMainExecutor;
 
+import java.net.URI;
+
 public class ROSTest {
-  private static RosCore rosCore;
+  //private static RosCore rosCore;
   private static DefaultNodeMainExecutor executor = (DefaultNodeMainExecutor) DefaultNodeMainExecutor.newDefault();
 
   public static void main(String[] args)
@@ -15,37 +17,35 @@ public class ROSTest {
     /*13111 is the ROS port. if you are launching ros files 
       from roscpp or rospy make sure to specify the argument '-p 13111'
       when you run the launch file.*/
-    rosCore = RosCore.newPublic(11311); 
-    rosCore.start();
+    //rosCore = RosCore.newPublic(11311);
+    //rosCore.start();
 
     try {
-      rosCore.awaitStart();
+      //rosCore.awaitStart();
 
+      URI masterUri = new URI("http://192.168.0.105:11311");
 
       Talker talker = new Talker();
       Listener listener = new Listener();
 
-      execute("Listener", listener);
-      execute("Talker", talker);
+      execute("Listener", listener, masterUri);
+      execute("Talker", talker, masterUri);
 
       Thread.sleep(2000);
 
-      executor.shutdownNodeMain(listener);
-
-      Thread.sleep(2000);
-
-      executor.shutdownNodeMain(talker);
+      executor.shutdown();
     }
     catch (Throwable e) {
       System.out.println("Error: " + e.getMessage());
     }
   }
 
-  private static void execute(String name, NodeMain node)
+  private static void execute(String name, NodeMain node, URI masterUri)
   {
     System.out.println("Starting " + name + " node...");
     NodeConfiguration config = NodeConfiguration.newPrivate();
-    config.setMasterUri(rosCore.getUri());
+    //config.setMasterUri(rosCore.getUri());
+      config.setMasterUri(masterUri);
     config.setNodeName(name);
     executor.execute(node, config);
   }
